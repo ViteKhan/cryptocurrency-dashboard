7fafe798105ff2758190b1a7807038905697ca75
ViteKhan <42898182+ViteKhan@users.noreply.github.com>
2023-06-11 03:05:51 +0500

    added Statistics component

---
+++ src/components/Statistics/Statistics.tsx
@@ -0,0 +1,22 @@
+import { FC } from 'react';
+import { Grid } from '@mui/material';
+
+import { AssetPlatforms } from './AssetPlatforms';
+import { Categories } from './Categories';
+import { Exchanges } from './Exchanges';
+import { MarketIndexes } from './MarketIndexes';
+
+export const Statistics: FC = () => {
+  const statistics = [<Categories/>, <Exchanges/>, <AssetPlatforms/>, <MarketIndexes/>];
+  return (
+    <>
+      {statistics.map((Component, idx) => (
+        <Grid key={idx} item lg={3} sm={6} xl={3} xs={12}>
+          {Component}
+        </Grid>
+      ))}
+    </>
+  );
+};
+
+export default Statistics;
--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,14 +1,17 @@
 import { FC } from 'react';
-import { Box } from '@mui/material';
+import { Container, Grid } from '@mui/material';
 
+import { Statistics } from 'components/Statistics';
 import { DashboardHeader } from './DashboardHeader';
 
 export const Dashboard: FC = () => {
 
   return (
-    <Box>
-      <DashboardHeader/>
-
-    </Box>
+    <Container maxWidth={false}>
+      <Grid container spacing={2}>
+        <DashboardHeader/>
+        <Statistics/>
+      </Grid>
+    </Container>
   );
 };
